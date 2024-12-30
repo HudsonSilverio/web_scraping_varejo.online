389ab6e3734feffb10addb62279eff42641dc26d
Hudson_Silverio <hudsonoksj@gmail.com>
2024-12-30 20:01:52 -0300

    teste

--- main.py
+++ main.py
@@ -2,24 +2,17 @@
 
 # print('ola')
 
-import os  # Importação não utilizada (flake8)g
+import os  # Importação não utilizada (flake8)
+import sys
 
 def funcao_com_erro_indice():
     lista = [1, 2, 3]
-    print(lista[3]) # IndexError (não detectado diretamente pelo pre-commit, precisa de testes unitários)
+    print(lista[3]) # IndexError (não detectado pelos hooks diretamente, precisa de teste unitário)
 
 
 def funcao_com_espacos_no_fim():
     print("Com espaços no fim.   ") # Espaços no fim da linha (trailing-whitespace)
 
-
-def funcao_com_linha_em_branco_no_fim():
-    print("Com linha em branco no fim.")
-
-#linha em branco no fim do arquivo (end-of-file-fixer)
-
-
-variavel_nao_usada = 10 # flake8: variável não utilizada
 
 def funcao_com_nome_longo_demais_para_flake8_excedendo_o_limite_de_79_caracteres(): # flake8: nome da função muito longo
     print("Função com nome longo")
@@ -27,7 +20,9 @@ def funcao_com_nome_longo_demais_para_flake8_excedendo_o_limite_de_79_caracteres
 def funcao_com_codigo_mal_formatado():
     if(1== 1):
         print("Mal formatado") # Black irá formatar isso
-
+    x = 1
+    y=2
+    print(x + y)
     
     
     
