ef92036bd1b7fe1d7dab0fcb128be072d90aeb30
Hudson_Silverio <hudsonoksj@gmail.com>
2025-01-03 15:27:02 -0300

    terminando as funcoes e fechando o o time 10s

--- .app/ETL/extract.py
+++ .app/ETL/extract.py
@@ -2,53 +2,46 @@
 # #print('hola')
 
 import requests
+import time 
 from bs4 import BeautifulSoup
 
 def pag_produto(): # Essa função será usada para acessar a página da web e obter seu conteúdo (o HTML da página). 
-    url = "https://www.amazon.com.br/Samsung-Smart-UHD-55DU7700-Processador/dp/B0CYNFLZ2B/ref=sr_1_2?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1MVX4ULH9V1E4&dib=eyJ2IjoiMSJ9.HR8umExreqtm6LLMStxYt02jVW_PQ3f7dB1kj3dMuCfVeItwNgAdtUjBUSuHW_zr1uUs1KhfR1eLhnOkCIY24ZNChpW9xLQUdAmOEd1DZw6lzw35roq04Rl3Or3V8Tr4_je56HOiOwrqD7-5qWpONUsfMM21VtfYxjpmx9Bs-OUzWXDs9xPct34ebfhAAQMlohWL9ANXUKYPcNBcRyZPXF7xhHgrTbtp-U_0sZBV6ezCcRnYDKfVBkR-T0CHhrD9ND1zN0GzpMtMegS0f4GR9v9Wp_bWwLeP5uAAyTj-YC9CUkrrPyjvwsDqCG8KVw4qlmblBpJeaCCVd2z8BvoG1QlIp1rIBCjycyM9WOPr_R1qU0GK2O-KPA85ONpMG4pOt721rx3HPT62vEECHB8TpMK9iU5RhJD_OipGajPk5EGMn5KPithFtrB0gVT7-O-k.LSpMQJZowel96Jobsi5nR8aeFu7jizYB_-INsY8HlWc&dib_tag=se&keywords=televis%C3%A3o+samsung+du7700+55&qid=1735728349&sprefix=televis%C3%A3o+samsung+du7700+55%2Caps%2C297&sr=8-2&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
+    url = "https://www.amazon.com.br/Samsung-Smart-Crystal-UHD-55DU8000/dp/B0CYN9P8TS/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dib=eyJ2IjoiMSJ9.ejoQJGWcp6_aW17EYE8TQo9GepQubyQjmd9POOyKWJrKBdrDpBTDHbKn1suasUCAb1fKUlNbmrmMBlTZ9qYk0oXqX9GUdHZPOpQ9800Fm6qDG99srlQIH79K3QW7GDYZM-qLkxxkHqajqYGWNzOgE1GdtIDXN0pN-BSXxWLQp3Zjs0TXEg7pfTIFNxWdJordPpWZtNffeuHEAUOCl-_Rv8lNPeX1Kau67NHg6KP6t8j7vS1ukGMFSXgQYaGaFFIuwQDzdIVJPoUo5mNjy2cbE06k9CTDPJmUQPs6f2rOqAM.Kq3Amm7LmNy51uf2ofTa4Kcc9QNGUqW0M7vkh0h6zJc&dib_tag=se&keywords=televisao+sansung+DU&qid=1735850371&sr=8-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
     response = requests.get(url)
     return response.text
 
 def parse_page(html): # funcao que coleta exatamente a parte que voce deseja do HTML
     soup = BeautifulSoup(html, 'html.parser')
-    nome_produto = soup.find('span',id="productTitle", class_='a-size-large product-title-word-break').get_text()
-    #preco_atual = soup.find('span', class_='a-price-whole').get_text()
-    #preco_antigo = soup.find('span', class_='a-offscreen').get_text()
+    preco_antigo = soup.find('span', class_='a-offscreen').get_text().replace('R$', ' ')
+    nome_produto = soup.find('span',id="productTitle", class_='a-size-large product-title-word-break').get_text().replace(' ', '')
+   
+    # codigo que separa as classes para poder pegar elemento especifico dentro de uma lista
+    categoria_1: list = soup.find_all('span', class_='a-price-whole')
+    preco_atual = (categoria_1[-2].get_text().replace(',', ''))
     
-    print(nome_produto)
-    #print(preco_atual, preco_antigo)
+    categoria_2: list = soup.find_all('span', class_='a-offscreen')
+    preco_antigo = (categoria_2[0].get_text()).replace('R$', '')
+    
+    
+    return {
+        'nome_produto': nome_produto,
+        'preco_atual' : preco_atual,
+        'preco_antigo': preco_antigo,
+        
+    }
+    
 
 if __name__ =='__main__': # Garante que o código abaixo seja executado apenas quando o script for rodado diretamente.
-    scrip_pagina = pag_produto() # Obtem o HTML da página para armazenar e usar no futuro.
-    parse_page(scrip_pagina)
-
-
-# import requests
-# from bs4 import BeautifulSoup
-
-# def pag_produto():  # Acessa a página da web e obtém o HTML.
-#     url = "https://www.amazon.com.br/Samsung-Smart-UHD-55DU7700-Processador/dp/B0CYNFLZ2B"
-#     response = requests.get(url)
-#     return response.text
-
-# def parse_page(html):  # Coleta a parte desejada do HTML.
-#     soup = BeautifulSoup(html, 'html.parser')
-#     # Salvar HTML em um arquivo para verificação
-#     with open('pagina.html', 'w', encoding='utf-8') as f:
-#         f.write(html)
-
-#     # Tentar encontrar o preço
-#     try:
-#         nome_produto = soup.find('span', class_='a-price-whole').get_text(strip=True)
-#         print("Preço encontrado:", nome_produto)
-#     except AttributeError:
-#         print("Preço não encontrado no HTML.")
-
-# if __name__ == '__main__':  # Executa o código.
-#     scrip_pagina = pag_produto()  # Obtem o HTML.
-#     parse_page(scrip_pagina)
+    while True: # parte do codigo que faz a automação do request da pagina automatico a cada 10''
+        conteudo_pag = pag_produto()
+        coleta = parse_page(conteudo_pag)
+        print(coleta)
+        time.sleep(10)  
 
 
     
+
+
+
 
     
