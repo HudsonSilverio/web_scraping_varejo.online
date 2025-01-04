beb44b5185d7635db1fcd1ac21818d870e29ab29
Hudson_Silverio <hudsonoksj@gmail.com>
2025-01-04 20:29:04 -0300

    salvando dentro do df

--- .app/ETL/transform.py
+++ .app/ETL/transform.py
@@ -10,9 +10,13 @@ def dataframe(coleta, df):
     return df
     
 if __name__ =='__main__': # Garante que o código abaixo seja executado apenas quando o script for rodado diretamente.
+    
+    df = pd.DataFrame()
+   
     while True: # parte do codigo que faz a automação do request da pagina automatico a cada 10''
         conteudo_pag = pag_produto()
         coleta = parse_page(conteudo_pag)
-        print(coleta)
+        df = dataframe(coleta, df)
+        print(df)
         time.sleep(10)  
 
