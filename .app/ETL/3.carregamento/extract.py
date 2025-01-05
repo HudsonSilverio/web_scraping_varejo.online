ac219a0b609c99f94e18a6785b1fbec9881d9297
Hudson_Silverio <hudsonoksj@gmail.com>
2025-01-05 18:04:35 -0300

    tentado corrijir o erro de conversao das variaveis para float

---
+++ .app/ETL/3.carregamento/extract.py
@@ -0,0 +1,50 @@
+# # poetry run python extract.py
+# #print('hola')
+    
+import requests
+import time 
+from bs4 import BeautifulSoup
+
+def pag_produto(): # Essa função será usada para acessar a página da web e obter seu conteúdo (o HTML da página). 
+    url = "https://www.amazon.com.br/Samsung-Smart-Crystal-UHD-55DU8000/dp/B0CYN9P8TS/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dib=eyJ2IjoiMSJ9.ejoQJGWcp6_aW17EYE8TQo9GepQubyQjmd9POOyKWJrKBdrDpBTDHbKn1suasUCAb1fKUlNbmrmMBlTZ9qYk0oXqX9GUdHZPOpQ9800Fm6qDG99srlQIH79K3QW7GDYZM-qLkxxkHqajqYGWNzOgE1GdtIDXN0pN-BSXxWLQp3Zjs0TXEg7pfTIFNxWdJordPpWZtNffeuHEAUOCl-_Rv8lNPeX1Kau67NHg6KP6t8j7vS1ukGMFSXgQYaGaFFIuwQDzdIVJPoUo5mNjy2cbE06k9CTDPJmUQPs6f2rOqAM.Kq3Amm7LmNy51uf2ofTa4Kcc9QNGUqW0M7vkh0h6zJc&dib_tag=se&keywords=televisao+sansung+DU&qid=1735850371&sr=8-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
+    response = requests.get(url)
+    return response.text
+
+def parse_page(html): # funcao que coleta exatamente a parte que voce deseja do HTML
+    soup = BeautifulSoup(html, 'html.parser')
+    preco_antigo = soup.find('span', class_='a-offscreen').get_text().replace('R$', ' ')
+    nome_produto = soup.find('span',id="productTitle", class_='a-size-large product-title-word-break').get_text().replace(' ', '')
+   
+    # codigo que separa as classes para poder pegar elemento especifico dentro de uma lista
+    categoria_1: list = soup.find_all('span', class_='a-price-whole')
+    preco_atual = (categoria_1[-2].get_text().replace(',', '').replace(',', '.'))
+    #preco_atual = float(preco_atual)
+    
+    categoria_2: list = soup.find_all('span', class_='a-offscreen')
+    preco_antigo = (categoria_2[0].get_text()).replace('R$', '').replace(',', '.')
+    #preco_antigo = float(preco_antigo)
+    
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S:') # cod para marcar a hora da coleta
+    
+    return {
+        'nome_produto': nome_produto,
+        'preco_atual' : preco_atual,
+        'preco_antigo': preco_antigo,
+        'timestamp' : timestamp
+    }
+    
+
+if __name__ =='__main__': # Garante que o código abaixo seja executado apenas quando o script for rodado diretamente.
+    while True: # parte do codigo que faz a automação do request da pagina automatico a cada 10''
+        conteudo_pag = pag_produto()
+        coleta = parse_page(conteudo_pag)
+        print(coleta)
+        time.sleep(10)  
+
+
+    
+
+
+
+
+    
---
+++ .app/ETL/3.carregamento/load.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import sqlite3
+import pandas as pd
+from extract import parse_page, pag_produto
+
+
+def criando_conexao(db_name='tv_sansung_prices.db'):
+    """Cria uma conexão com o banco de dados SQLite."""
+     # Define o caminho do banco de dados na pasta 'data'
+    conn = sqlite3.connect(db_name)
+    return conn
+
+def configuracao_df(conn):
+    """Cria a tabela de preços se ela não existir."""
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS prices (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_name TEXT,
+            nome_produto INTEGER,
+            preco_atual INTEGER,
+            preco_antigo INTEGER,
+            timestamp TEXT
+        )
+    ''')
+    conn.commit()
+
+def save_to_database(conn, data):
+    """Salva uma linha de dados no banco de dados SQLite usando pandas."""
+    df = pd.DataFrame([data])  # Converte o dicionário em um DataFrame de uma linha
+    df.to_sql('prices', conn, if_exists='append', index=False)  # Salva no banco de dados
+
+def maximo_venda(coon): #funcao que conecta com o DB e retorna o preco maximo naquele momento
+    
+    cursor = conn.cursor()
+    cursor.execute("SELECT MAX(preco_atual), timestamp FROM prices")
+    result = cursor.fetchone()
+    if result and result[0] is not None:
+        return result[0], result[1]
+    return None, None
+
+# Teste das funções
+if __name__ == '__main__':
+    # Configuração do banco de dados
+    conn = criando_conexao()
+    configuracao_df(conn)
+
+    while True:
+        # Faz a requisição e parseia a página
+        conteudo_pag = pag_produto()
+        coleta = parse_page(conteudo_pag)
+        current_price = coleta['preco_atual']
+        
+        # Obtém o maior preço já salvo
+        max_price, max_price_timestamp = maximo_venda(conn)
+        
+         # Comparação de preços
+        if max_price is None or current_price > max_price:
+            print(f"Preço maior detectado: {current_price}")
+            max_price = current_price  # Atualiza o maior preço
+            max_price_timestamp = coleta['timestamp']  # Atualiza o timestamp do maior preço
+        else:
+            print(f"O maior preço registrado é {max_price} em {max_price_timestamp}")
+        
+        # Salva os dados no banco de dados SQLite
+        save_to_database(conn, coleta)
+        print("Dados salvos no banco:", coleta)
+        
+        # Aguarda 10 segundos antes da próxima execução
+        time.sleep(10)
+
+    # Fecha a conexão com o banco de dados
+    conn.close()
